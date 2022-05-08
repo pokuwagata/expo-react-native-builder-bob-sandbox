29900d446305966e25c56c5a4b200eb3d3e69204
pokuwagata <pokuwagata@gmail.com>
2022-05-09 00:28:24 +0900

    ios works

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { NativeModules, StyleSheet, Text, View } from "react-native";
+import { multiply } from "react-native-awesome-module";
+import "expo-dev-client";
 
 export default function App() {
+  // const { AwesomeModule } = NativeModules;
+  const [result, setResult] = useState<number | undefined>();
+
+  useEffect(() => {
+    // AwesomeModule.multiply(3, 7).then(setResult);
+    multiply(4, 7).then(setResult);
+  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const ans = await multiply(2, 2);
+  //     // console.log(1);
+  //   })();
+  // }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{result}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
